872dcadcabbc2e66b592409b48fe03657ae8da0a
Admir Saraseli <asaraseli@gmail.com>
2020-06-08 17:57:46 +0200

    update ad.ex

--- Advanced Exercise/Admir/js/admir.js
+++ Advanced Exercise/Admir/js/admir.js
@@ -5,10 +5,10 @@ for (var i = 0 ; i < booklistJson.length; i++) {
       var obj = booklistJson[i];
       if (obj.read === "true") {
       booksdata = booksdata+"<li><figure><img src='img/"+booklistJson[i].cover+"'><figcaption style='color: red'>"+
-      "The title is: "+obj.title+" and the author is: "+obj.author+"</figcaption></figure></li>";
+      "The title is: "+obj.title+" and the author is: "+obj.author+"</figcaption></figure></li>"+"<hr>";
 	 }	else {
 	 	booksdata = booksdata+"<li><figure><img src='img/"+booklistJson[i].cover+"'><figcaption >"+
-	 	"The title is: "+obj.title+" and the author is: "+obj.author+"</figcaption></figure></li>";
+	 	"The title is: "+obj.title+" and the author is: "+obj.author+"</figcaption></figure></li>"+"<hr>";
 	 }
       
   }
