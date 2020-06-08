2f639dbb3f4d47d97ba9905273a52c6ff75aa35b
Admir Saraseli <asaraseli@gmail.com>
2020-06-08 10:20:15 +0200

    basic Ex

--- Basic Exercise/js/admir.js
+++ Basic Exercise/js/admir.js
@@ -4,6 +4,9 @@ var fries = `{ "fries_size": "Large French Fries", "calories": "570" }`;
 var sandata = JSON.parse(sandwiches);
 var fridata = JSON.parse(fries);
 
-document.write("My favorite sandwich is a Hamburger which has approximately "
-	+sandata.calories+" calories, along with it I enjoy eating "+fridata.fries_size+" which have about "
-	+fridata.calories+" calories.")
+document.write(
+	"My favorite sandwich is a Hamburger which has approximately "+
+	sandata.calories+" calories, along with it I enjoy eating "+
+	fridata.fries_size+" which have about "+
+	fridata.calories+" calories."
+)
